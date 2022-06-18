0e19e48f4f66299bb9a5789a24f259a064201753
Thad Ras <thadras@gmail.com>
2022-06-18 15:04:41 +1200

    Add waypoints, image toggle and code formatting

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ import bezierEasing from "bezier-easing";
 import rectCrop from "rect-crop";
 import button from "./ui"
 import { fabric } from "fabric";
-// import Cropzone from './cropzone';
-// import extend from 'tui-code-snippet/object/extend';
 
 
 //#region Utility methods
 // Utility to load an Image by url, which works data-urls
 const loadCrossOriginImage = (src) => new Promise((success, failure) => {
-    let img = new window.Image();
-    img.crossOrigin = true;
-    img.onload = () => success(img);
-    img.onabort = img.onerror = failure;
-    img.src = src;
-    image = img
+  let img = new window.Image();
+  img.crossOrigin = true;
+  img.onload = () => success(img);
+  img.onabort = img.onerror = failure;
+  img.src = src;
+  image = img
 });
 
 // Utility to create a promise resolved after a delay
@@ -28,43 +26,45 @@ const title = t => {
 };
 
 function simpleArraySum(ar) {
-    var sum = 0;
-    for (var i = 0; i < ar.length; i++) {
-      sum += ar[i];
-    }
-    return sum;
+  var sum = 0;
+  for (var i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
 }
 
 function recordingLength() {
-    return simpleArraySum(durations) + simpleArraySum(delays);
+  return simpleArraySum(durations) + simpleArraySum(delays);
 }
 
 function displayArray(data) {
-    let msg = ""
-    data.forEach((value, key) => {
-      msg += `[${key}]: ${value}; `
-    })
-    console.log(msg);
+  let msg = ""
+  data.forEach((value, key) => {
+    msg += `[${key}]: ${value}; `
+  })
+  console.log(msg);
 }
 
 function clearKBData() {
-    centerPoints.splice(0, centerPoints.length);
-    zoom.splice(0, zoom.length);
-    durations.splice(0, durations.length);
-    delays.splice(0, delays.length);
-    crops.splice(0, crops.length);
-    easings.splice(0, easings.length);
-    cropzones.splice(0, cropzones.length);
-    continueLoop = false;
-    canvas.remove()
-    dumpKBData();
-    console.groupCollapsed('drawing the clean slate')
-    console.log(image)
-    imgContext.drawImage(image, 0, 0);
-    console.groupEnd()
+  centerPoints.splice(0, centerPoints.length);
+  zoom.splice(0, zoom.length);
+  durations.splice(0, durations.length);
+  delays.splice(0, delays.length);
+  crops.splice(0, crops.length);
+  easings.splice(0, easings.length);
+  cropzones.splice(0, cropzones.length);
+  continueLoop = false;
+  canvas.remove()
+  dumpKBData();
+  console.groupCollapsed('drawing the clean slate')
+  console.log(image)
+  imgContext.drawImage(image, 0, 0);
+  console.groupEnd()
+  waypoints.innerHTML = ""
+  imageToggle(null)
 }
 
-function rectangleBounds(center, zoom){
+function rectangleBounds(center, zoom) {
   var viewportRatio = 1
   var dimensionRatio = imgDeminsions.width / imgDeminsions.height;
 
@@ -85,29 +85,67 @@ function rectangleBounds(center, zoom){
 }
 
 function dumpKBData() {
-    console.groupCollapsed(`${zoom.length} centerPoints, zoom, durations, delay, cropzones sum ${recordDuration}`)
-    displayArray(centerPoints);
-    displayArray(zoom);
-    displayArray(durations);
-    displayArray(delays);
-    displayArray(cropzones);
-    console.groupEnd()
+  console.groupCollapsed(`${zoom.length} centerPoints, zoom, durations, delay, cropzones sum ${recordDuration}`)
+  displayArray(centerPoints);
+  displayArray(zoom);
+  displayArray(durations);
+  displayArray(delays);
+  displayArray(cropzones);
+  console.groupEnd();
+  dispalyWaypoints();
+
 }
+
+function dispalyWaypoints() {
+  waypoints.innerHTML = ""
+  for (var i = 0; i < centerPoints.length; i++) {
+    const div = document.createElement("div");
+    div.className = "column"
+    const p1 = document.createElement("p");
+    let text = `${i}: c[${centerPoints[i][0]}, ${centerPoints[i][1]}]; z: ${zoom[i]}`
+    p1.innerText = text;
+    p1.className = "items"
+    const p2 = document.createElement("p");
+    text = `duration:${durations[i] / 1000}; delay:${delays[i] / 1000}`
+    p2.className = "items"
+    p2.innerText = text;
+    div.appendChild(p1)
+    div.appendChild(p2)
+    waypoints.appendChild(div)
+    waypoints.appendChild(document.createElement("br"))
+  }
+
+}
+
+function imageToggle(ev) {
+  const div = document.getElementById("target")
+  var style  = div.style
+  console.log(style)
+  if (style.display == "none" || !ev) {
+    // Always show image when loading image or clearing KB data
+    style.display = "flex"
+    Object.apply(div.style, style)  
+    return
+  }
+  style.display = "none"
+  Object.apply(div.style, style)  
+}
+
 // Ken Burns animation worker
 const exampleAnimation =
   kenBurns => // for a given kenBurns instance (impl agnostic)
-  source => // and the 'source' (image / texture, dep on the impl)
-  crops.reduce((p, crop, i) => p.then(() => // chain all the steps
-    kenBurns.animate( // animate kenburns
-      source,
-      crop,
-      crops[(i + 1) % crops.length], // next crop spot
-      durations[i],
-      easings[i]
-    )
-    .then(() => delay(delays[i])) // wait a bit
-  ), Promise.resolve()) // start with a resolved promise
-  .then(() => continueLoop && exampleAnimation(kenBurns)(source)); // loop again
+    source => // and the 'source' (image / texture, dep on the impl)
+      crops.reduce((p, crop, i) => p.then(() => // chain all the steps
+        kenBurns.animate( // animate kenburns
+          source,
+          crop,
+          crops[(i + 1) % crops.length], // next crop spot
+          durations[i],
+          easings[i]
+        )
+          .then(() => delay(delays[i])) // wait a bit
+      ), Promise.resolve()) // start with a resolved promise
+        .then(() => continueLoop && exampleAnimation(kenBurns)(source)); // loop again
 //#endregion
 
 //#region  Animation structures and data
@@ -119,7 +157,7 @@ const centerPoints = [
   [0.54, 0.47],
   [0.81, 0.48],
 ];
-const zoom= [
+const zoom = [
   0.4,
   0.3,
   0.1,
@@ -128,145 +166,134 @@ const zoom= [
 const cropzones = [];
 // Frame transition easing
 const easings = [
-    bezierEasing(0.6, 0, 1, 1),
-    bezierEasing(0, 0, 1, 1),
-    bezierEasing(0.8, 0, 0.2, 1),
-    bezierEasing(0.5, 0, 1, 1),
-    bezierEasing(0, 0, 0.6, 1),
-  ];
-  // Duration of easing between frames
-  const durations = [
-    3000,
-    2000,
-    3000,
-    2000,
-    3000,
-  ];
-  // Delay on freeze-frame
-  const delays = [
-    800,
-    300,
-    0,
-    1000,
-    0,
-  ];
-  // Actual croppings of the image
-  const crops = [ 
-    rectCrop(zoom[0], centerPoints[0]),
-    rectCrop(zoom[1], centerPoints[1]),
-    rectCrop(zoom[2], centerPoints[2]),
-    rectCrop(zoom[3], centerPoints[3]),
-    rectCrop.largest,
-  ];
-  let recordDuration = recordingLength();
-  let continueLoop = true;
+  bezierEasing(0.6, 0, 1, 1),
+  bezierEasing(0, 0, 1, 1),
+  bezierEasing(0.8, 0, 0.2, 1),
+  bezierEasing(0.5, 0, 1, 1),
+  bezierEasing(0, 0, 0.6, 1),
+];
+// Duration of easing between frames
+const durations = [
+  3000,
+  2000,
+  3000,
+  2000,
+  3000,
+];
+// Delay on freeze-frame
+const delays = [
+  800,
+  300,
+  0,
+  1000,
+  0,
+];
+// Actual croppings of the image
+const crops = [
+  rectCrop(zoom[0], centerPoints[0]),
+  rectCrop(zoom[1], centerPoints[1]),
+  rectCrop(zoom[2], centerPoints[2]),
+  rectCrop(zoom[3], centerPoints[3]),
+  rectCrop.largest,
+];
+let recordDuration = recordingLength();
+let continueLoop = true;
 
 //#endregion KB points
 
 // describe our loop example
 let exampleImageUrl = "http://i.imgur.com/Uw2EQEk.jpg";
 
-function exportVid(blob) {
-  const vid = document.createElement('video');
-  vid.src = URL.createObjectURL(blob);
-  vid.controls = true;
-  container.appendChild(vid);
-  const a = document.createElement('a');
-  a.download = 'myvid.webm';
-  a.href = vid.src;
-  a.textContent = 'download the video';
-  container.appendChild(a);
-}
-function anim(){
+function anim() {
   requestAnimationFrame(anim);
 }
 
 var canvas;  // Ken Burns canvas for rendering
 const createKBCanvas = () => {
-    // Canvas2D example
-    var canvas2d = document.createElement("canvas");
-    canvas2d.style.width = "400px";
-    canvas2d.style.height = "400px";
-    canvas2d.width = 800;
-    canvas2d.height = 800;
-    return canvas2d
+  // Canvas2D example
+  var canvas2d = document.createElement("canvas");
+  canvas2d.style.width = "400px";
+  canvas2d.style.height = "400px";
+  canvas2d.width = 800;
+  canvas2d.height = 800;
+  return canvas2d
 }
 
 function doImageMapping(imageUrl) {
-    exampleImageUrl = imageUrl;
-    if (canvas) {
-        canvas.remove()
-    }
-    canvas = createKBCanvas()
+  exampleImageUrl = imageUrl;
+  if (canvas) {
+    canvas.remove()
+  }
+  canvas = createKBCanvas()
 
-    const ctx = canvas.getContext("2d");
-    // resetCanvas(ctx)
-    side.appendChild(canvas);
-    // console.log(row)
-    var kenBurnsCanvas2d = new KenBurnsCanvas2D(ctx);
-    console.log('starting KB map')
-    loadCrossOriginImage(imageUrl, true)
+  const ctx = canvas.getContext("2d");
+  // resetCanvas(ctx)
+  side.appendChild(canvas);
+  // console.log(row)
+  var kenBurnsCanvas2d = new KenBurnsCanvas2D(ctx);
+  console.log('starting KB map')
+  loadCrossOriginImage(imageUrl, true)
     .then(exampleAnimation(kenBurnsCanvas2d))
-    .catch(e => console.error("Canvas2D implementation failure:", e)); 
-    
-    // Load full image with on-click handler
-    loadCrossOriginImage(imageUrl).then(img => {
-        console.log(
-          'loaded the image  with w: %d by h: %d',
-          img.width,
-          img.height,
-        );
-    
-        // imageData = image
-        imgDeminsions.width = img.width;
-        imgDeminsions.height = img.height;
-        imgCanvas.width = img.width
-        imgCanvas.height = img.height
-        imgContext.drawImage(img, 0, 0);
-        // var _canvas =  new fabric.Canvas(imageUrl, {
-        //   width: image.width,
-        //   height: image.height,
-          
-        //   // backgroundImage: imageData,
-        //   containerClass: 'canvas-wrap',
-        //   enableRetinaScaling: false,
-        //   interactive: true, 
-        // });
-        for (var i = 0; i < zoom.length; i++) {
-          wrectCrop(zoom[i], centerPoints[i], color[i%color.length]);
-        }
+    .catch(e => console.error("Canvas2D implementation failure:", e));
 
-        imgCanvas.addEventListener('click', canvasClick, false);
-    });
+  // Load full image with on-click handler
+  loadCrossOriginImage(imageUrl).then(img => {
+    console.log(
+      'loaded the image  with w: %d by h: %d',
+      img.width,
+      img.height,
+    );
+
+    // imageData = image
+    imgDeminsions.width = img.width;
+    imgDeminsions.height = img.height;
+    imgCanvas.width = img.width
+    imgCanvas.height = img.height
+    imgContext.drawImage(img, 0, 0);
+    // var _canvas =  new fabric.Canvas(imageUrl, {
+    //   width: image.width,
+    //   height: image.height,
+
+    //   // backgroundImage: imageData,
+    //   containerClass: 'canvas-wrap',
+    //   enableRetinaScaling: false,
+    //   interactive: true, 
+    // });
+    for (var i = 0; i < zoom.length; i++) {
+      wrectCrop(zoom[i], centerPoints[i], color[i % color.length]);
+    }
+
+    imgCanvas.addEventListener('click', canvasClick, false);
+  });
 }
 
 const createCropZone = (i) => {
-    const rectBounds = rectangleBounds(centerPoints[i], zoom[i])
-    //CanvasRect.strokeRect(x: number, y: number, w: number, h: number): void
-    console.log(`adding CZ ${JSON.stringify(rectBounds)}`)
-    const rect = new fabric.Rect(
-      {
-        originX: "left",
-        originY: "top",
-        top: rectBounds[0],
-        left: rectBounds[1],
-        width:rectBounds[2],
-        height: rectBounds[3],
-        strokeWidth: 1, // {@link https://github.com/kangax/fabric.js/issues/2860}
-        cornerSize: 10,
-        cornerColor: 'black',
-        borderColor: color[i%(color.length)],
-        // selectionBackgroundColor: 'lightblue',
-        fill: 'none',
-        hasRotatingPoint: false,
-        hasBorders: true,
-        lockScalingFlip: true,
-        lockRotation: true,
-        lockSkewingX: true,
-        lockSkewingY: true,
-      },
-    )
-    return rect;
+  const rectBounds = rectangleBounds(centerPoints[i], zoom[i])
+  //CanvasRect.strokeRect(x: number, y: number, w: number, h: number): void
+  console.log(`adding CZ ${JSON.stringify(rectBounds)}`)
+  const rect = new fabric.Rect(
+    {
+      originX: "left",
+      originY: "top",
+      top: rectBounds[0],
+      left: rectBounds[1],
+      width: rectBounds[2],
+      height: rectBounds[3],
+      strokeWidth: 1, // {@link https://github.com/kangax/fabric.js/issues/2860}
+      cornerSize: 10,
+      cornerColor: 'black',
+      borderColor: color[i % (color.length)],
+      // selectionBackgroundColor: 'lightblue',
+      fill: 'none',
+      hasRotatingPoint: false,
+      hasBorders: true,
+      lockScalingFlip: true,
+      lockRotation: true,
+      lockSkewingX: true,
+      lockSkewingY: true,
+    },
+  )
+  return rect;
 
 }
 
@@ -288,13 +315,13 @@ var wrectCrop = (zoom, center, color) => {
   var centerX = imgDeminsions.width * center[0];
   var centerY = imgDeminsions.height * center[1];
 
-  var rect= [
+  var rect = [
     (centerX - zoomedCanvasSize[0] / 2) > 0 ? centerX - zoomedCanvasSize[0] / 2 : 0,
     (centerY - zoomedCanvasSize[1] / 2) > 0 ? centerY - zoomedCanvasSize[1] / 2 : 0,
     zoomedCanvasSize[0],
     zoomedCanvasSize[1]
   ];
-//   console.log(`${color} rect ${JSON.stringify(rect)} z ${zoomedCanvasSize}`)
+  //   console.log(`${color} rect ${JSON.stringify(rect)} z ${zoomedCanvasSize}`)
   imgContext.beginPath();
 
   imgContext.strokeStyle = color;
@@ -303,21 +330,22 @@ var wrectCrop = (zoom, center, color) => {
   imgContext.fillStyle = color;
   imgContext.fill();
   imgContext.lineWidth = 5;
-  imgContext.strokeRect( rect[0],rect[1],rect[2],rect[3])
+  imgContext.strokeRect(rect[0], rect[1], rect[2], rect[3])
 
 }
+const randomHundreth = () => Math.floor(10 + Math.random() * 90) / 100
 var canvasClick = (ev) => {
   var cX = ev.offsetX / imgCanvas.width
   var cY = ev.offsetY / imgCanvas.height
-  centerPoints.push([cX, cY])
+  centerPoints.push([Math.floor(cX * 100) / 100, Math.floor(cY * 100) / 100])
   const wayPoints = zoom.length;
-  zoom.push(Math.random())
+  zoom.push(randomHundreth())
   console.log(`Click at cX ${cX} cY:${cY}`)
-  easings.push(easings[Math.floor(Math.random()*wayPoints)]);
+  easings.push(easings[Math.floor(Math.random() * wayPoints)]);
   // Duration of easing between frames
-  durations.push(Math.random()*10000)
+  durations.push((randomHundreth() * 10000).toFixed(2))
   // Delay on freeze-frame
-  delays.push(Math.random()*1000)
+  delays.push((randomHundreth() * 1000).toFixed(2))
   crops.push(rectCrop(zoom[wayPoints], centerPoints[wayPoints]));
   continueLoop = true;
   recordDuration = recordingLength();
@@ -332,12 +360,13 @@ const newImage = (image) => {
   // imageData = image;
   exampleImageUrl = image
   clearKBData()
-
+  imageToggle(null)
   doImageMapping(image)
 }
 
 //#region append elements to the DOM
 const row = document.createElement("div");
+const waypoints = document.getElementById("waypoints");
 const header = document.getElementById("header");
 const easel = document.getElementById("target");
 const container = document.getElementById("side");
@@ -350,28 +379,31 @@ doImageMapping(exampleImageUrl)
 controls.appendChild(title("Buttons"))
 controls.appendChild(row);
 row.appendChild(button.select(newImage))
-row.appendChild(button('dataDump', dumpKBData))
-row.appendChild(button('startRecording', startRecording))
 row.appendChild(button('Clear Data', clearKBData))
+row.appendChild(button('Toggle Image', imageToggle))
+row.appendChild(button('dataDump', dumpKBData))
 row.appendChild(button('ReyKey Data', () => doImageMapping(exampleImageUrl)))
+row.appendChild(button('startRecording', startRecording))
+dispalyWaypoints()
 //#endregion
 
 //#region WebM helper methods
+
 function startRecording() {
-    anim()
-    const chunks = []; // here we will store our recorded media chunks (Blobs)
-    const stream = canvas.captureStream(); // grab our canvas MediaStream
-    const rec = new MediaRecorder(stream); // init the recorder
-    // every time the recorder has new data, we will store it in our array
-    rec.ondataavailable = e => chunks.push(e.data);
-    // only when the recorder stops, we construct a complete Blob from all the chunks
-    rec.onstop = e => exportVid(new Blob(chunks, {type: 'video/webm'}));
-  
-    rec.start();
-    console.log(`started recording for ${recordDuration} s`)
-    setTimeout(()=>rec.stop(), recordDuration); // stop recording
-  }
-  
+  anim()
+  const chunks = []; // here we will store our recorded media chunks (Blobs)
+  const stream = canvas.captureStream(); // grab our canvas MediaStream
+  const rec = new MediaRecorder(stream); // init the recorder
+  // every time the recorder has new data, we will store it in our array
+  rec.ondataavailable = e => chunks.push(e.data);
+  // only when the recorder stops, we construct a complete Blob from all the chunks
+  rec.onstop = e => exportVid(new Blob(chunks, { type: 'video/webm' }));
+
+  rec.start();
+  console.log(`started recording for ${recordDuration} s`)
+  setTimeout(() => rec.stop(), recordDuration); // stop recording
+}
+
 function exportVid(blob) {
   const vidDiv = document.createElement('div');
   const vid = document.createElement('video');
@@ -385,7 +417,20 @@ function exportVid(blob) {
   controls.appendChild(a);
   container.appendChild(vidDiv);
 }
-function anim(){
+
+function anim() {
   requestAnimationFrame(anim);
+}
+
+function exportVid(blob) {
+  const vid = document.createElement('video');
+  vid.src = URL.createObjectURL(blob);
+  vid.controls = true;
+  container.appendChild(vid);
+  const a = document.createElement('a');
+  a.download = 'myvid.webm';
+  a.href = vid.src;
+  a.textContent = 'download the video';
+  container.appendChild(a);
 }
 //#endregion
--- ui.js
+++ ui.js
@@ -1,86 +1,85 @@
-function button (text, handler) {
-    const button = document.createElement("button");
-    button.addEventListener('click', handler);
-    button.innerText = text;
-    return button;
+function button(text, handler) {
+  const button = document.createElement("button");
+  button.addEventListener('click', handler);
+  button.innerText = text;
+  return button;
+}
 
+
+button.select = (loaded) => {
+  const handler = (ev) => {
+    console.log('You selected the file %s', ev.target.files[0].name);
+    createImageFromBlob(ev.target.files[0], loaded)
   }
-  
+  const label = document.createElement("label")
+  label.for = "userFile"
+  const input = document.createElement("input")
+  input.type = 'file';
+  input.accept = "image/*";
+  input.id = "userFile"
+  input.addEventListener('change', handler)
+  input.style = { display: "none" }
+  // label.appendChild(button("Select File", input.change))
+  label.appendChild(input)
 
- button.select = (loaded) => {
-    const handler = (ev) => {
-        console.log('You selected the file %s', ev.target.files[0].name);
-        createImageFromBlob(ev.target.files[0], loaded)
-    }
-    const label = document.createElement("label")
-    label.for = "userFile"
-    const input = document.createElement("input")
-    input.type = 'file';
-    input.accept = "image/*";
-    input.id = "userFile"
-    input.addEventListener('change', handler)
-    input.style = {display: "none"}
-    // label.appendChild(button("Select File", input.change))
-    label.appendChild(input)
+  return label;
+}
 
-    return label;
- } 
-  
- function createImageFromBlob(image, loaded) {
-    const reader = new FileReader();
-    const type = image.type.split('/');
-    if ('image' !== type[0]) {
-   
-      return;
-    }
-    reader.addEventListener('load', doFileRead.bind(this, reader, loaded), false);
-    if (image) {
-      reader.readAsDataURL(image);
-    }
+function createImageFromBlob(image, loaded) {
+  const reader = new FileReader();
+  const type = image.type.split('/');
+  if ('image' !== type[0]) {
+
+    return;
   }
+  reader.addEventListener('load', doFileRead.bind(this, reader, loaded), false);
+  if (image) {
+    reader.readAsDataURL(image);
+  }
+}
 
- const doFileRead = (reader, loaded) => {
-    
-    const image = new Image();
-    image.src = reader.result;
-    const vw = Math.max(
-      document.documentElement.clientWidth,
-      window.innerWidth || 0
+const doFileRead = (reader, loaded) => {
+
+  const image = new Image();
+  image.src = reader.result;
+  const vw = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  const vh = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  );
+  let imgDeminsionsX = vw;
+  let imgDeminsionsY = vh;
+  const imgCanvas = document.getElementById(
+    'canvas'
+  );
+  const ctx = imgCanvas.getContext('2d');
+  var scale = 1;
+  image.addEventListener('load', () => {
+    console.log(
+      'loaded the image with w: %d by h: %d, view w: %d h: %d',
+      image.width, image.height, vw, vh,
     );
-    const vh = Math.max(
-      document.documentElement.clientHeight,
-      window.innerHeight || 0
-    );
-    let imgDeminsionsX = vw;
-    let imgDeminsionsY = vh;
-    const imgCanvas = document.getElementById(
-        'canvas'
-      ) ;
-    const ctx = imgCanvas.getContext('2d');
-    var scale = 1;
-    image.addEventListener('load', () => {
-      console.log(
-        'loaded the image with w: %d by h: %d, view w: %d h: %d',
-        image.width, image.height, vw, vh,
+    if (vw < image.width && vh < image.height) {
+      scale = Math.max(vw / image.width, vh / image.height);
+      console.log('scale factor of  %s for viewport %s %s', scale, vw, vh);
+      imgDeminsionsX = Math.floor(
+        image.width * scale
       );
-      if (vw < image.width && vh < image.height) {
-        scale = Math.max(vw / image.width, vh / image.height);
-        console.log('scale factor of  %s for viewport %s %s', scale, vw, vh);
-        imgDeminsionsX = Math.floor(
-          image.width * scale
-        );
-        imgDeminsionsY = Math.floor(
-          image.height * scale
-        );
-      } else {
-        imgDeminsionsX = image.width;
-        imgDeminsionsY = image.height;
-      }
-      imgCanvas.width = imgDeminsionsX
-      imgCanvas.height = imgDeminsionsY
-      ctx.scale(scale, scale);
-      ctx.drawImage(image, 0, 0);
-      loaded(image.src)
-    });
-  }
-  module.exports = button;
+      imgDeminsionsY = Math.floor(
+        image.height * scale
+      );
+    } else {
+      imgDeminsionsX = image.width;
+      imgDeminsionsY = image.height;
+    }
+    imgCanvas.width = imgDeminsionsX
+    imgCanvas.height = imgDeminsionsY
+    ctx.scale(scale, scale);
+    ctx.drawImage(image, 0, 0);
+    loaded(image.src)
+  });
+}
+module.exports = button;
