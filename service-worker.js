c06587864e53c68379bc5ad4ac22e124fdf2d713
thadras <59382727+thadras@users.noreply.github.com>
2022-06-26 05:30:07 +0000

    Deploying to gh-pages from @ thadras/animage@135c90f5d782de448b9f321beabf91fbfe2b6cb7 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,28 @@
-const $CACHE_STORE = 'animage';
-const $NAVIGATION_FALLBACK = 'index.html';
+const VERSION = "0.1"
+const APP_PREFIX = "animage_"
+const CACHE_STORE = APP_PREFIX + VERSION
+const NAVIGATION_FALLBACK = 'index.html';
 
-const $FILES = ['bundle.js', 'gif.js', 'gif.worker.js', 'index.css', 'index.html', 'tree.jfif'];
+const FILES = [
+  '/animage/bundle.js',
+  '/animage/gif.js',
+  '/animage/gif.worker.js',
+  '/animage/index.css',
+  '/animage/index.html',
+  '/animage/tree.jfif'
+];
 
-const navigateFallback = $NAVIGATION_FALLBACK;
+const navigateFallback = NAVIGATION_FALLBACK;
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open($CACHE_STORE)
+    caches.open(CACHE_STORE)
       .then(cache => {
-        return cache.addAll($FILES);
+        console.log(`installing cache ${CACHE_STORE}`)
+        return cache.addAll(FILES);
       })
       .then(() => {
+        console.log('Skip waiting when installing cache')
         return self.skipWaiting();
       })
   );
@@ -19,53 +30,56 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open($CACHE_STORE)
-      .then(cache => {
-        return cache.keys()
-          .then(cacheNames => {
-            return Promise.all(
-              cacheNames.filter(cacheName => {
-                return $FILES.indexOf(cacheName) === -1;
-              }).map(cacheName => {
-                return caches.delete(cacheName);
-              })
-            );
-          })
-          .then(() => {
-            return self.clients.claim();
-          });
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
       })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_STORE)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
+  console.log('fetch request : ' + event.request.url)
   if (event.request.method === 'GET') {
     let url = event.request.url.indexOf(self.location.origin) !== -1 ?
       event.request.url.split(`${self.location.origin}/`)[1] :
       event.request.url;
-    let isFileCached = $FILES.indexOf(url) !== -1;
+    let isFileCached = FILES.indexOf(url) !== -1;
 
     // This is important part if your app needs to be available offline
     // If request wasn't found in array of files and this request has navigation mode and there is defined navigation fallback
     // then navigation fallback url is picked istead of real request url
     if (!isFileCached && event.request.mode === 'navigate' && navigateFallback) {
       url = navigateFallback;
-      isFileCached = $FILES.indexOf(url) !== -1;
+      isFileCached = FILES.indexOf(url) !== -1;
     }
 
     if (isFileCached) {
       event.respondWith(
-        caches.open($CACHE_STORE)
+        caches.open(CACHE_STORE)
           .then(cache => {
             return cache.match(url)
               .then(response => {
                 if (response) {
+                  console.log('fetched from cache')
                   return response;
                 }
                 throw Error('There is not response for such request', url);
               });
           })
           .catch(error => {
+            console.log('fetching from network')
             return fetch(event.request);
           })
       );
